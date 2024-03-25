47706438f070bcb9e4018b04cef1eca10584b753
MuhammadLuqmanSawati <143633750+MuhammadLuqmanSawati@users.noreply.github.com>
2024-03-25 16:58:48 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        name: "num1",
+        type: "number",
+        message: " please enter first number: ",
+    },
+    {
+        name: "num2",
+        type: "number",
+        message: "please enter second number: ",
+    },
+    {
+        name: "operator",
+        type: "list",
+        message: "Please select the operator",
+        choices: ["Addition", "Subtraction", "multiplication", "Division"],
+        // choices: ["+","-","*","/"],
+    },
+]);
+// let number1 = answers.num1;
+// let number2 = answers.num2;
+// let opt = answers.operator;
+// console.log(answers);
+// console.log(number2);
+// console.log(opt);
+if (answers.operator === "Addition") {
+    console.log(answers.num1 + answers.num2);
+}
+else if (answers.operator === "Multiplication") {
+    console.log(answers.num1 * answers.num2);
+}
+else if (answers.operator === "Subtraction") {
+    console.log(answers.num1 - answers.num2);
+}
+else if (answers.operator === "Division") {
+    console.log(answers.num1 / answers.num2);
+}
+else {
+    "Kindly select any one operator";
+}
